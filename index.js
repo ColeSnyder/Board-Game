ffb0a5cc5877d39217bbe4f2cf82b7caf2f93eb4
Ham80234 <you@example.comHam80234@gmail.com>
2019-02-07 18:59:44 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var fs =  require('fs');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+io.on('connection', function(socket){
+  
+  console.log('a user connected');
+  io.emit('chat message', msg = 'a user has connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    io.emit('chat message', msg = 'a user has disconnected');
+  });
+});
+  
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+      io.emit('chat message', msg);
+    });
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
