d70a796981892d94ef01b6fb49ef203d749d7fdb
ColeSnyder <33235561+ColeSnyder@users.noreply.github.com>
2019-02-09 16:44:22 -0600

    Added jquery and conncted socket io file in ejs file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var fs =  require('fs');
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
  const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
@@ -11,15 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
-const router = require('./routes/index.js')
+const indexRouter = require('./routes/index.js')
 
-app.use(router);
+app.use(indexRouter);
   
 app.get('*', function(req, res) {
   res.render('error');
 });
 
 
-http.listen(3001, function(){
+server.listen(3001, function(){
   console.log('listening on *:3001');
 });
