c3474a67b421488667d1cd775ac0e0563018ca26
ColeSnyder <33235561+ColeSnyder@users.noreply.github.com>
2019-02-11 15:34:40 -0600

    took away separate routes function and it now routes straight in the app.js file. Works

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
- const path = require('path');
+const path = require('path');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -11,10 +11,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
-const player1router = require('./routes/player1route.js');
-const player2router = require('./routes/player2route.js');
-const player3router = require('./routes/player3route.js');
-const player4router = require('./routes/player4route.js');
+app.get('/player1', function(req, res){
+  console.log('Routing to index1.ejs')
+ res.render('player1');
+});
+app.get('/player2', function(req, res){
+  console.log('Routing to index2.ejs')
+ res.render('player2');
+});
+app.get('/player3', function(req, res){
+  console.log('Routing to index3.ejs')
+ res.render('player3');
+});
+app.get('/player4', function(req, res){
+  console.log('Routing to index4.ejs')
+ res.render('player4');
+});
+
+//*********************************************************************************************
+
+// const player1router = require('./routes/player1route.js');
+// const player2router = require('./routes/player2route.js');
+// const player3router = require('./routes/player3route.js');
+// const player4router = require('./routes/player4route.js');
 
 //The following 4 lines are what we use to route each player. The first player to join 
 //uses 'app.use(player1router)' and so forth until all players are routed to their respective 
@@ -23,12 +42,14 @@ const player4router = require('./routes/player4route.js');
 // app.use(player2router);
 // app.use(player3router);
 // app.use(player4router);
+
+//*********************************************************************************************
   
 app.get('*', function(req, res) {
   res.render('error');
 });
 
-
 server.listen(3001, function(){
   console.log('listening on *:3001');
+  // app.use(player1router);
 });
