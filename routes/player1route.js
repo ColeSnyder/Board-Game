dd3ae3b1a1eb8fc0e4bf907a9073f1b237f0a60d
Ham80234 <you@example.comHam80234@gmail.com>
2019-02-11 16:35:20 -0600

    Deleted Route added name to time tracker

--- routes/player1route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', function(req, res){
-    console.log('Routing to index1.ejs')
-   res.render('player1');
-});
-module.exports = router
--- routes/player2route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('/', function(req, res){
-    console.log('Routing to index2.ejs')
-   res.render('index2');
-});
-module.exports = router
