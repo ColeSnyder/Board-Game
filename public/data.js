dcc1f5f0d0478d9648dcb6253a9489ee39a6f95c
Ham80234 <you@example.comHam80234@gmail.com>
2019-02-13 13:20:29 -0600

    updates some functions

--- public/data.js
+++ public/data.js
@@ -3,44 +3,40 @@
 
 /*********************************** Player 1 data ****************************************/
 
-function RandomNum() {  
-    var random =  Math.floor(Math.random() * 20) + 1 
-    return random
-}
+var Players = [];
 
-var player1PublicData = [];
 
-for (i=0; i<3; i++) {
-   player1PublicData.push(RandomNum());
-}
+var P1 = CreatePlayer("P1");
+FillHand(P1);
+console.log(P1.Hand);
+
+
 
 
 /*********************************** Player 2 data ****************************************/
 
-var player2PublicData = [];
+var P2 = CreatePlayer("P2");
+FillHand(P2);
+console.log(P2.Hand);
 
-for (i=0; i<3; i++) {
-    player2PublicData.push(RandomNum());
- }
 
 
 /*********************************** Player 3 data ****************************************/
 
-var player3PublicData = [];
+var P3 = CreatePlayer("P3");
+FillHand(P3);
+console.log(P3.Hand);
 
-for (i=0; i<3; i++) {
-    player3PublicData.push(RandomNum());
- }
 
 
 /*********************************** Player 4 data ****************************************/
 
-var player4PublicData = [];
+var P4 = CreatePlayer("P4");
+FillHand(P4);
+console.log(P4.Hand);
 
-for (i=0; i<3; i++) {
-    player4PublicData.push(RandomNum());
- }
 
+/*********************************** Functions  ****************************************/
 function CreatePlayer(Name) {  
     var Player = [];
     Player.name = Name;
@@ -49,8 +45,14 @@ function CreatePlayer(Name) {
     return Player;
 }
 
-function ComapareHands() {  
-
+function Comapare(arr1, arr2) {  
+    for (let i = 0; i < arr1.length; i++) {
+        const Current = arr1[i]
+        for(let j = 0; j < arr2.length; j++) {
+            const element = arr2[j];
+            
+        }
+    }
 }
 
 function AddNumber() {
@@ -58,8 +60,45 @@ function AddNumber() {
 }
 
 function join(Player) {
-
+    // Very basic and will probably change 
+    if(Players.length == 3){
+        console.log("Player area full")
+    }
+    if(Players.length == 0){
+        Players.push(P1);
+    }
+    if(Players.length == 1){
+        Players.push(P2);
+    }
+    if(Players.length == 2){
+        Players.push(P3);
+    }
+    if(Players.length == 3){
+        Players.push(P4);
+    }
 }
 function Disconnect(Player) {
+ // no idea how we want to impliment this 
+}
 
+function RedOrGreen(Compare, Hand) {  
+    for (let i = 0; i < Players.length; i++) {
+        const element = Players[i];
+        for (let j = 0; j < Players.length; j++) {
+            const element = Players[j];
+            
+        }
+        
+    }
+}
+
+function RandomNum() {  
+    var random =  Math.floor(Math.random() * 20) + 1 
+    return random
+}
+
+function FillHand(player) {  
+   for (let i = 0; i < 3    ; i++) {
+        player.Hand.push(RandomNum())    
+   }
 }
