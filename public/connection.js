1b34337b7f08b9dfdfa77baaafc56c8547bdb004
Steven McVicker <smcvicker14@winona.edu>
2019-02-20 18:45:01 -0600

    index styling

--- public/connection.js
+++ public/connection.js
@@ -1,12 +1,13 @@
 var playerNum;
 var ColesIPHome = "192.168.0.16";
 var ColesIPWSU = "10.19.80.107";
+var StevensIPHome = "192.168.0.17";
 
-var socket = io.connect(ColesIPHome + ":3001");
-<<<<<<< HEAD
-var socket2 = io.connect(ColesIPHome + ":3001/LoadPool.html");
-=======
->>>>>>> 98bf4552fabe2ce9e9ebc20bc9beb6cc96b41817
+var socket = io.connect(StevensIPHome + ":3001");
+
+var socket2 = io.connect(StevensIPHome + ":3001/LoadPool.html");
+
+
 
 // Query DOM
 var btn = document.getElementById("joinGameButton");
@@ -16,17 +17,17 @@ var message = document.getElementById("message");
 btn.addEventListener('click', function() {
     socket.emit('joinRequest', {
         message: message.value
-    }); 
-    window.location.href = "http://" + ColesIPHome + ":3001/LoadPool.html";
+    });
+    window.location.href = "http://" + StevensIPHome + ":3001/LoadPool.html";
 });
 
 // join.addEventListener('click', function() {
 //     socket.emit('OtherObject', {
 //         input: input.value
-//     }); 
+//     });
 // });
 
-//Listen for events 
+//Listen for events
 socket.on('joinRequest', function(data) {
     console.log("into last socket");
     // var playerNum = data.num;
