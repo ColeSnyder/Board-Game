f8c842ec63ceeb0197941cba03ba6f1678cb20cd
Steven McVicker <smcvicker14@winona.edu>
2019-02-27 18:59:29 -0600

    currentplayer

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var fs =  require('fs');
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 const path = require('path');
-var Player= [];
+var Player = [];
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -12,106 +12,106 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/player1', function(req, res){
-  console.log('Routing to index1.ejs')
- res.render('player1');
+app.get('/player1', function(req, res) {
+    console.log('Routing to index1.ejs')
+    res.render('player1');
 });
-app.get('/player2', function(req, res){
-  console.log('Routing to index2.ejs')
- res.render('player2');
+app.get('/player2', function(req, res) {
+    console.log('Routing to index2.ejs')
+    res.render('player2');
 });
-app.get('/player3', function(req, res){
-  console.log('Routing to index3.ejs')
- res.render('player3');
+app.get('/player3', function(req, res) {
+    console.log('Routing to index3.ejs')
+    res.render('player3');
 });
-app.get('/player4', function(req, res){
-  console.log('Routing to index4.ejs')
- res.render('player4');
+app.get('/player4', function(req, res) {
+    console.log('Routing to index4.ejs')
+    res.render('player4');
 });
 
 app.get('*', function(req, res) {
-  res.render('error');
+    res.render('error');
 });
 
-server.listen(3001, '0.0.0.0', function(){
-  console.log('listening on *:3001');
+server.listen(3001, '0.0.0.0', function() {
+    console.log('listening on *:3001');
 });
 
 
 // Everything below this line will have to do with Socket.io *********************************************
 
 io.on('connection', function(socket) {
-  console.log("Made socket connection " + socket.id)
+    console.log("Made socket connection " + socket.id)
 
-  socket.on('object', function(data) {
-    playerArray.push("player" + playerArray.length);
-    console.log(playerArray.length);
-    var num = playerArray.length;
-      io.to(socket.id).emit("player"+num, num);
-  });
-
-  socket.on('P1Number', function(data) {
-    Player[0].Hand.push(data);
-    console.log(Player)
-    console.log("hand is " +Player[0].Hand);
-
-    //call compare function
-    Comapare(Player[0], Player[1], Player[2], Player[3]);
-
-    //save response from compare to variable
-
-    io.emit('P1Number', {
-      data,
-      //whatever the other variable is
+    socket.on('object', function(data) {
+        playerArray.push("player" + playerArray.length);
+        console.log(playerArray.length);
+        var num = playerArray.length;
+        io.to(socket.id).emit("player" + num, num);
     });
-  });
 
-  socket.on('P2Number', function(data) {
-    Player[1].Hand.push(data);
-    console.log(Player)
-    console.log(Player[1]);
-    io.emit('P2Number', data);
-  });
+    socket.on('P1Number', function(data) {
+        Player[0].Hand.push(data);
+        console.log(Player)
+        console.log("hand is " + Player[0].Hand);
 
-  socket.on('P3Number', function(data) {
-    Player[2].Hand.push(data);
-    console.log(Player)
-    console.log(Player[2]);
-    io.emit('P3Number', data);
-    
-  });
-  socket.on('P4Number', function(data) {
-    Player[3].Hand.push(data);
-    console.log(Player)
-    console.log(Player[3]);
-    io.emit('P4Number', data);
-    
-  });
+        //call compare function
+        Comapare(Player[0], Player[1], Player[2], Player[3]);
 
-  socket.on('sendPlayer', function(data) {
-    Player.push(data);
-    console.log(Player);
-  });
-  
-// if(Player.length == 3 ){
-//   console.log("sending traffic....")
-//   io.emit("getarray", {
-//     Player
-//   });
-// }
+        //save response from compare to variable
 
-socket.on('Player1Wins', function(data) {
-    data.points++
-});
-socket.on('Player2Wins', function(data) {
-  data.points++
-});
-socket.on('Player3Wins', function(data) {
-  data.points++
-});
-socket.on('Player4Wins', function(data) {
-  data.points++
-});
+        io.emit('P1Number', {
+            data,
+            //whatever the other variable is
+        });
+    });
+
+    socket.on('P2Number', function(data) {
+        Player[1].Hand.push(data);
+        console.log(Player)
+        console.log(Player[1]);
+        io.emit('P2Number', data);
+    });
+
+    socket.on('P3Number', function(data) {
+        Player[2].Hand.push(data);
+        console.log(Player)
+        console.log(Player[2]);
+        io.emit('P3Number', data);
+
+    });
+    socket.on('P4Number', function(data) {
+        Player[3].Hand.push(data);
+        console.log(Player)
+        console.log(Player[3]);
+        io.emit('P4Number', data);
+
+    });
+
+    socket.on('sendPlayer', function(data) {
+        Player.push(data);
+        console.log(Player);
+    });
+
+    // if(Player.length == 3 ){
+    //   console.log("sending traffic....")
+    //   io.emit("getarray", {
+    //     Player
+    //   });
+    // }
+
+    socket.on('Player1Wins', function(data) {
+        data.points++
+    });
+    socket.on('Player2Wins', function(data) {
+        data.points++
+    });
+    socket.on('Player3Wins', function(data) {
+        data.points++
+    });
+    socket.on('Player4Wins', function(data) {
+        data.points++
+    });
 
 });
 // Player position array ********************************************************************************
@@ -135,12 +135,12 @@ socket.on('Player4Wins', function(data) {
 //       Winner: Player[3]
 //   })
 // }
-function Comapare(player1, player2, player3, player4) {
-  var firstCompare = player2.Hand.every(val => player1.Hand.includes(val));
-  var secondCompare = player3.Hand.every(val => player1.Hand.includes(val));
-  var thirdCompare = player4.Hand.every(val => player1.Hand.includes(val));
-  
-  
+function Comapare(currentPlayer, player2, player3, player4) {
+    var firstCompare = player2.Hand.every(val => currentPlayer.Hand.includes(val));
+    var secondCompare = player3.Hand.every(val => currentPlayer.Hand.includes(val));
+    var thirdCompare = player4.Hand.every(val => currentPlayer.Hand.includes(val));
+
+
 
 
 }
@@ -172,16 +172,16 @@ function Comapare(player1, player2, player3, player4) {
 
 
 for (let i = 0; i < Player.length; i++) {
-  const current = Player[i];
-  for (let j = 0; j < Player.length; j++) {
-    const element = Player[j];
-    var Token =  current.Name+"-"+element.Name
-      if(Comapare(current.Hand, element.Hand)){
-        io.on("Change", {
-          Token
-        })
-      }
-  }
+    const current = Player[i];
+    for (let j = 0; j < Player.length; j++) {
+        const element = Player[j];
+        var Token = current.Name + "-" + element.Name
+        if (Comapare(current.Hand, element.Hand)) {
+            io.on("Change", {
+                Token
+            })
+        }
+    }
 }
 
 
