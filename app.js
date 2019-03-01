58419497dd82cdb2b337906844f9899022e00d95
Steven McVicker <smcvicker14@winona.edu>
2019-03-01 00:04:51 -0600

    more points updates

--- app.js
+++ app.js
@@ -194,7 +194,7 @@ function Comapare(currentPlayer, temp2, temp3, temp4) {
     var firstCompare = temp2.Hand.every(val => currentPlayer.Hand.includes(val));
     if (firstCompare == true) {
         match = "p" + currentPlayer.ID + "p" + temp2.ID;
-        currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
+        //currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
         if (currentPlayer.roundPoints == 4) {
             //end round
         }
@@ -203,7 +203,7 @@ function Comapare(currentPlayer, temp2, temp3, temp4) {
     var secondCompare = temp3.Hand.every(val => currentPlayer.Hand.includes(val));
     if (secondCompare == true) {
         match = "p" + currentPlayer.ID + "p" + temp3.ID;
-        currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
+        // currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
         if (currentPlayer.roundPoints == 4) {
             //end round
         }
@@ -213,7 +213,7 @@ function Comapare(currentPlayer, temp2, temp3, temp4) {
     var thirdCompare = temp4.Hand.every(val => currentPlayer.Hand.includes(val));
     if (thirdCompare == true) {
         match = "p" + currentPlayer.ID + "p" + temp4.ID;
-        currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
+        //currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
         if (currentPlayer.roundPoints == 4) {
             //end round
         }
@@ -227,52 +227,53 @@ function Comapare(currentPlayer, temp2, temp3, temp4) {
         console.log("Player 3 points:" + Player[2].roundPoints);
         console.log("Player 4 points:" + Player[3].roundPoints);
 
-        if (match == "p1p2" && p1p2 == false) {
+        if (match == "p1p2" && p1p2 === false) {
             p1p2 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p1p3" && p1p3 == false) {
+        } else if (match == "p1p3" && p1p3 === false) {
             p1p3 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p1p4" && p1p4 == false) {
+        } else if (match == "p1p4" && p1p4 === false) {
             p1p4 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p2p1" && p2p1 == false) {
+        } else if (match == "p2p1" && p2p1 === false) {
             p2p1 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p2p3" && p2p3 == false) {
+        } else if (match == "p2p3" && p2p3 === false) {
             p2p3 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p2p4" && p2p4 == false) {
+        } else if (match == "p2p4" && p2p4 === false) {
             p2p4 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p3p1" && p3p1 == false) {
+        } else if (match == "p3p1" && p3p1 === false) {
             p3p1 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p3p2" && p3p2 == false) {
+        } else if (match == "p3p2" && p3p2 === false) {
             p3p2 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p3p4" && p3p4 == false) {
+        } else if (match == "p3p4" && p3p4 === false) {
             p3p4 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p4p1" && p4p1 == false) {
+        } else if (match == "p4p1" && p4p1 === false) {
             p4p1 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p4p2" && p4p2 == false) {
+        } else if (match == "p4p2" && p4p2 === false) {
             p4p2 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
-        }
-        if (match == "p4p3" && p4p3 == false) {
+        } else if (match == "p4p3" && p4p3 === false) {
             p4p3 = true;
+            currentPlayer.roundPoints = currentPlayer.roundPoints + 1;
             return match;
         }
 
